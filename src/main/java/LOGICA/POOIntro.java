02ae6508fc3c86eeae6b18d2fb735ff87c2cc10b
Aidem1262 <josevimontiel@gmail.com>
2026-04-19 15:35:17 -0600

    Commit 2

--- src/main/java/LOGICA/POOIntro.java
+++ src/main/java/LOGICA/POOIntro.java
@@ -11,6 +11,6 @@ package LOGICA;
 public class POOIntro {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
     }
 }
